bf232f65c1efc7867620a84b38f32f393932d87e
Nicolás Fantone <nfantone@gmail.com>
2016-09-08 15:19:16 +0100

    Add CI build scripts

---
+++ Scripts/install.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+BASE_URL=http://beta.unity3d.com/download
+HASH=741887044bba
+VERSION=5.5.0b2
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
