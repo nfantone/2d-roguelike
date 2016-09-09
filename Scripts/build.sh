3c048e54025faa8b71c3fe27ba3ce6306a2fd71f
Nicolás Fantone <nfantone@gmail.com>
2016-09-09 15:58:12 +0100

    Apply minor stylistic changes to build script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,26 +1,26 @@
 #! /bin/sh
 
-project="2d-roguelike"
+PROJECT="2d-roguelike"
 
-echo "Attempting to build $project for Windows"
+echo "Attempting to build [$PROJECT] for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$PROJECT.exe" \
   -quit
 
-echo "Attempting to build $project for OS X"
+echo "Attempting to build [$PROJECT] for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$PROJECT.app" \
   -quit
 
-echo 'Logs from build'
+echo 'Showing logs from build'
 cat $(pwd)/unity.log
