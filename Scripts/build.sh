38aabf54aee5e9e4ec295b8c94a0527c5891eae4
Nicolás Fantone <nfantone@gmail.com>
2016-09-09 15:55:27 +0100

    Remove Linux build attempt

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -22,15 +22,5 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
-
 echo 'Logs from build'
 cat $(pwd)/unity.log
